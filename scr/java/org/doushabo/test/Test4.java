43588be73060147640ba941d38549e80c5396f67
Doushabo <2824868962@qq.com>
2021-08-10 19:16:29 +0800

    add testtest45

--- scr/java/org/doushabo/test/Test4.java
+++ scr/java/org/doushabo/test/Test4.java
@@ -3,5 +3,6 @@ package java.org.doushabo.test;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("test45");
+        System.out.println("test4455");
     }
 }
