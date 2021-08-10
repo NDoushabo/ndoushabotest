af1c47535d44cb56109c2f2c4bbb63b5c60ab373
Doushabo <2824868962@qq.com>
2021-08-10 19:00:09 +0800

    zaishishi

--- scr/java/org/doushabo/test/Doushabo3.java
+++ scr/java/org/doushabo/test/Doushabo3.java
@@ -3,6 +3,6 @@ package java.org.doushabo.test;
 public class Doushabo3 {
     public static void main(String[] args) {
         System.out.println("this is test file3");
-
+        System.out.println("zaishishi");
     }
 }
